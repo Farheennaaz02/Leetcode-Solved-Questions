527158c1ceb7d5456716ed1fcbe9fc5e2400c302
FARHEEN NAAZ <farheenreshma577@gmail.com>
2026-06-27 00:05:52 +0530

    Time: 0 ms (100%), Space: 50.1 MB (29.12%) - LeetHub

---
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public IList<string> BuildArray(int[] target, int n) {
+        int j =0;
+        int i =0;
+        List <int>  arr = new ();
+        List < string > result = new ();
+
+        for ( int k =1 ; k<=n;k++){
+            arr.Add(k);
+        }
+        while ( i <target.Length){
+            if ( arr[j]==target[i]){
+                result.Add( "Push");
+                i++;
+                j++;
+
+            }
+            else {
+                result.Add("Push");
+                result.Add("Pop");
+                j++;
+
+            }
+
+        }
+        
+            return result ;
+
+
+        
+    }
+}
