64dd76d426b7a6e51002c3486ec911d5aaa41579
FARHEEN NAAZ <farheenreshma577@gmail.com>
2026-06-26 23:16:21 +0530

    Time: 10 ms (40.43%), Space: 48.2 MB (39.29%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cs
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cs
@@ -1,10 +1,8 @@
 public class Solution {
     public int[] SmallerNumbersThanCurrent(int[] nums) {
         int [] result = new int [nums.Length];
-        
         for ( int i =0;i<nums.Length ;i++){
             int count =0;
-
             int j=0;
             if ( i ==j){
                 j++;
@@ -12,13 +10,11 @@ public class Solution {
             while ( j<nums.Length){
                 if ( nums[i]> nums[j]){
                     count ++;
-                    
                 }
                 j++;
             }
             result[i]= count ;
         }
         return result ;
-        
     }
 }
