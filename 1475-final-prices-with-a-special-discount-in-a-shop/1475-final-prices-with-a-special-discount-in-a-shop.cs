a31c2224633a05d8689120823a2962a89bf05042
FARHEEN NAAZ <farheenreshma577@gmail.com>
2026-06-30 21:21:12 +0530

    Time: 4 ms (18.54%), Space: 48.1 MB (16.72%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int[] FinalPrices(int[] prices) {
+        List <int > result = new ();
+        int n = prices.Length ;
+        for ( int i =0;i<n;i++){
+            bool found = false;
+            for ( int j = i+1 ; j<n ;j++){
+                if (prices[j]<=prices[i]){
+                    found=true;
+                    result.Add(prices[i]-prices[j]);
+                    break;
+                }
+                
+            }
+            if (!found){
+                result.Add(prices[i]);
+            }
+
+        }
+        return result.ToArray ();
+        
+    }
+}
