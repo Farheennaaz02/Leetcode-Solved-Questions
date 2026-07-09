d084250a2bf5e6d5188e1633563620cd431314c8
FARHEEN NAAZ <farheenreshma577@gmail.com>
2026-07-09 23:13:47 +0530

    Time: 1 ms (44.2%), Space: 63.2 MB (80.87%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public void Rotate(int[] nums, int k) {
+        int n = nums.Length ;
+        k%=n;
+        int [] result = new int [n];
+        int index =0;
+        for ( int i =n-k;i<n;i++){
+            result[index]= nums[i];
+            index++;
+        }
+        for ( int i=0;i<n-k;i++){
+            result[index]= nums[i];
+            index ++;
+        }
+        for ( int i =0;i<n ;i++){
+            nums[i]= result [i];
+        }
+    }
+}
