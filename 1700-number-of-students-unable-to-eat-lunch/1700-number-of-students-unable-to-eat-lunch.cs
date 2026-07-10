1e6ba49a106986849b7ecc8f3f95ed46f1a280e8
FARHEEN NAAZ <farheenreshma577@gmail.com>
2026-07-10 22:10:58 +0530

    Time: 1 ms (71.14%), Space: 41.1 MB (75.62%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int CountStudents(int[] students, int[] sandwiches) {
+        Queue<int> queue= new Queue<int > (students);
+        int index =0;
+        int count =0;
+        while (queue.Count>0){
+            int std = queue.Dequeue();
+            if ( std == sandwiches[index]){
+                index++;
+                count =0;
+            }
+            else{
+                queue.Enqueue(std);
+                count ++;
+            }
+            if ( count  == queue.Count ){
+            break ;
+        }
+        }
+        
+        return queue.Count ;
+
+
+
+        
+    }
+}
