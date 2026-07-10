937580caf194b20cebb729655020f1549a07955a
FARHEEN NAAZ <farheenreshma577@gmail.com>
2026-07-10 22:40:51 +0530

    Time: 0 ms (100%), Space: 41.5 MB (71.29%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int TimeRequiredToBuy(int[] tickets, int k) {
+        int time =0;
+        for ( int i =0;i<tickets.Length;i++){
+            if ( i <=k){
+                time+=Math.Min (tickets[i],tickets[k]);
+            }
+            else{
+                time +=Math.Min(tickets[i],tickets[k]-1);
+            }
+        }
+        return time;
+        
+    }
+}
