3dd53b13484040da19c5eb67aa17f032bf8da587
FARHEEN NAAZ <farheenreshma577@gmail.com>
2026-08-12 22:37:41 +0530

    Time: 4 ms (42.79%), Space: 61.9 MB (92.34%) - LeetHub

--- 0443-string-compression/0443-string-compression.cs
+++ 0443-string-compression/0443-string-compression.cs
@@ -1,29 +1,27 @@
 public class Solution {
     public int Compress(char[] chars) {
-        int i =0;
-        int index =0;
-        int n = chars.Length ;
-        while(i<n){
+        int i=0;
+        int index=0;
+        int n =chars.Length;
+        
+        while (i<n){
             char currentchar = chars[i];
             int count =0;
-            while (i<n&& chars[i]==currentchar){
+            while(i<n&& chars[i]==currentchar){
+                count++;
                 i++;
-                count ++;
-
             }
-            
             chars[index]= currentchar;
-            index ++;
-            if (count >1){
+            index++;
+            if (count>1){
                 string countstr = count.ToString();
-                foreach (char ch in countstr){
+                foreach ( char ch in countstr){
                     chars[index]=ch;
-                    index ++;
+                    index++;
                 }
             }
         }
-        return index ;
-
+        return index;
         
     }
 }
