4b467fde0a353a6404c1b87480a718bb12bf581a
FARHEEN NAAZ <farheenreshma577@gmail.com>
2026-08-12 21:20:25 +0530

    Time: 1 ms (99.64%), Space: 65.4 MB (8.36%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public bool IncreasingTriplet(int[] nums) {
+        int first=int.MaxValue;
+        int second = int.MaxValue;
+        foreach ( int num in nums){
+            if ( num<=first){
+                first = num;
+            }
+            else if ( num>first && num<=second){
+                second = num;
+            }
+            else {
+                return true ;
+            }
+        }
+        return false ;
+
+        
+    }
+}
