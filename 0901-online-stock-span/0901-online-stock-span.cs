2047c646f23a9f4bf87e5a3906cc38cee0c9a635
FARHEEN NAAZ <farheenreshma577@gmail.com>
2026-08-18 22:49:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.cs
@@ -0,0 +1,25 @@
+public class StockSpanner {
+     Stack<(int price , int span)> stack;
+
+    public StockSpanner() {
+        stack = new Stack<(int price , int span)> ();
+
+        
+    }
+    public int Next(int price) {
+        int span =1;
+        while ( stack.Count >0&& stack.Peek().price <=price){
+            span += stack.Pop().span;
+        }
+
+        stack.Push((price , span));
+        return span;
+        
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.Next(price);
+ */
