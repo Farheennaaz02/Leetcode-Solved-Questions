b8f468fb8adf81095cd09d485c26551a73ce34f8
FARHEEN NAAZ <farheenreshma577@gmail.com>
2026-08-22 16:22:58 +0530

    Time: 8 ms (93.25%), Space: 45.3 MB (26.99%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cs
+++ 0437-path-sum-iii/0437-path-sum-iii.cs
@@ -1,30 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 public class Solution {
-
     public int PathSum(TreeNode root, int targetSum) {
-        if (root == null) {
-            return 0;
-        }
-
-        return DFS(root, targetSum)
-             + PathSum(root.left, targetSum)
-             + PathSum(root.right, targetSum);
+        Dictionary <long , int > map = new ();
+        map[0]= 1;
+        return DFS ( root , targetSum , 0,map);
+        
     }
-
-    int DFS(TreeNode root, long targetSum) {
-
-        if (root == null) {
-            return 0;
+    int DFS ( TreeNode root , int targetsum , long currentsum , Dictionary <long , int > map){
+        if ( root == null){
+            return 0 ;
         }
-
-        int count = 0;
-
-        if (root.val == targetSum) {
-            count++;
+        currentsum += root.val;
+        int count =0;
+        if ( map.ContainsKey(currentsum-targetsum)){
+            count += map[currentsum - targetsum];
         }
+        if ( map.ContainsKey(currentsum)){
+            map[currentsum ]++;
 
-        count += DFS(root.left, targetSum - root.val);
-        count += DFS(root.right, targetSum - root.val);
-
-        return count;
+        }
+        else {
+            map[currentsum]=1;
+        }
+        count += DFS ( root.left , targetsum , currentsum , map);
+        count += DFS ( root.right , targetsum , currentsum , map);
+        map[currentsum]--;
+        return count ;
     }
 }
