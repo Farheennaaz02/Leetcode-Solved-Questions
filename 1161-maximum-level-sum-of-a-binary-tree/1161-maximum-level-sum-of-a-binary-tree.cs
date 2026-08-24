15717df59a607375741e5fc84fb3c0b1dced01c7
FARHEEN NAAZ <farheenreshma577@gmail.com>
2026-08-24 22:26:04 +0530

    Time: 5 ms (65.74%), Space: 51.9 MB (93.52%) - LeetHub

---
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cs
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int MaxLevelSum(TreeNode root) {
+        // print the number off  the level with the max sum 
+        // bfs and level sum ceck
+        // update the maxum 
+       
+        int level =1;
+        int maxsum = int.MinValue;
+        int ans =1;
+        if ( root == null){
+            return 0;
+        }
+        Queue<TreeNode> q = new ();
+        q.Enqueue(root);
+        while ( q.Count >0){
+            int size = q.Count ;
+             int sum =0;
+            
+            
+            for ( int i =0;i<size ;i++){
+               
+                TreeNode node = q.Dequeue();
+                sum+= node.val;
+
+                if ( node.left!= null){
+                    q.Enqueue(node.left);
+                }
+                if ( node.right != null){
+                    q.Enqueue(node.right);
+                }
+            }
+            if ( sum >maxsum){
+                maxsum = sum ;
+                ans = level;
+            }
+            level ++;
+        }
+        return   ans;
+        
+    }
+}
