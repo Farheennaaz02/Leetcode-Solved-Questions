f3dd25aa8795137056e35e2e5e4e86f8f60623da
FARHEEN NAAZ <farheenreshma577@gmail.com>
2026-08-25 21:14:28 +0530

    Time: 125 ms (70.13%), Space: 52.4 MB (86.39%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cs
+++ 0200-number-of-islands/0200-number-of-islands.cs
@@ -1,29 +1,31 @@
 public class Solution {
     public int NumIslands(char[][] grid) {
-        int count =0;
-        if ( grid==null || grid.Length ==0 || grid[0].Length ==0){
+        if ( grid==null|| grid[0].Length== 0|| grid.Length ==0){
             return 0;
         }
-        for ( int i =0;i<grid.Length ;i++){
-            for ( int j =0;j<grid[0].Length ;j++){
+        int count =0;
+        int  n = grid.Length ;
+        int m = grid[0].Length ;
+        for ( int i =0;i<n;i++){
+            for ( int j=0;j<m;j++){
                 if (grid[i][j]=='1'){
-                    dfs(grid , i, j);
+                    DFS(grid , i , j, n ,m);
                     count ++;
                 }
+             
             }
         }
         return count ;
-
         
     }
-    private void dfs (char [][] grid , int i , int j ){
-        if ( i<0|| i>=grid.Length|| j <0|| j>=grid[0].Length || grid[i][j]=='0'){
+    void DFS (char [] []  grid , int i ,int j, int n , int m  ){
+        if ( i<0||j<0||i>=n||j>=m||grid[i][j]=='0'){
             return ;
         }
         grid[i][j]='0';
-        dfs ( grid , i+1 , j);
-        dfs (grid , i-1, j);
-        dfs ( grid , i, j+1);
-        dfs ( grid , i,j-1);
+        DFS ( grid , i+1 ,j,n,m);
+        DFS ( grid , i-1,j,n,m);
+        DFS ( grid , i, j-1,n,m);
+        DFS ( grid , i, j+1,n,m);
     }
 }
