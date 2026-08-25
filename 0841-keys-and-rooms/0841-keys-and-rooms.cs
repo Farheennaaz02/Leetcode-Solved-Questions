d099c5ab2d2fc00c204b1845126694156bd21065
FARHEEN NAAZ <farheenreshma577@gmail.com>
2026-08-25 20:36:26 +0530

    Time: 1 ms (99.52%), Space: 47.7 MB (46.15%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cs
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cs
@@ -1,26 +1,26 @@
 public class Solution {
-    bool [] visited ;
-
+    bool [] visited;
     public bool CanVisitAllRooms(IList<IList<int>> rooms) {
         int n = rooms.Count;
-        visited = new bool [n];
-         DFS ( 0, rooms);
-        for ( int i =0;i<n;i++){
-            if ( !visited[i]){
-                return false;
+        visited = new bool[n];
+        DFS ( 0 , rooms);
+        for( int i =0;i<n;i++){
+            if (!visited[i]){
+                return false ;
             }
         }
         return true;
 
+
         
     }
-    public void DFS ( int room , IList<IList<int>> rooms){
-        visited[room]= true;
+    void DFS ( int room , IList<IList<int>> rooms){
+        visited[room]= true ;
         foreach ( int nextroom in rooms[room]){
-            if (!visited[nextroom ]){
+            if (!visited[nextroom]){
                 DFS(nextroom , rooms);
+
             }
         }
-    
     }
 }
