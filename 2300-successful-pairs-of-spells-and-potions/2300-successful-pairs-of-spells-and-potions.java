7fdcc1ce2e341588bfeaf96d070df1f894ece025
FARHEEN NAAZ <farheenreshma577@gmail.com>
2026-09-10 14:59:40 +0530

    Time: 51 ms (46.67%), Space: 147.1 MB (19.33%) - LeetHub

---
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        Arrays.sort (potions);
+        int [] ans = new int [spells.length ];
+        for ( int i =0;i<spells.length;i++){
+            int left =0;
+            int right = potions.length-1;
+            while ( left <= right){
+                int mid = left+(right - left)/2;
+                long product= (long) spells[i]*potions[mid];
+                if (product >= success){
+                    // km krna pdega 
+                    right = mid -1;
+                }
+                else {
+                    left = mid +1;
+                }
+                ans[i]= potions.length - left ;// left beacuse vo chte honge 
+
+            }
+        }
+        return ans;
+
+        
+    }
+}
