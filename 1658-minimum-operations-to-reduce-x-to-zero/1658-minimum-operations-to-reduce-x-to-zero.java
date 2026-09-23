14569f8f512f06142d2c645a745be9cd63224406
FARHEEN NAAZ <farheenreshma577@gmail.com>
2026-09-23 20:17:20 +0530

    Time: 4 ms (98.07%), Space: 102.2 MB (44.57%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minOperations(int[] nums, int x) {
+        int total=0;
+        for ( int i =0;i<nums.length;i++){
+            total+=nums[i];
+        }
+        int target = total-x;
+        if (target <0){
+            return -1;
+        }
+        int left =0;
+        int sum =0;
+        int maxlen=-1;
+        for ( int right =0;right<nums.length;right ++){
+            sum +=nums[right];
+            while (sum >target){
+                sum-=nums[left];
+                left++;
+            }
+            if (sum==target){
+                maxlen = Math.max(maxlen , right -left +1);
+                
+            }
+        }
+        if (maxlen==-1){
+            return -1;
+        }
+        return nums.length-maxlen;
+        
+    }
+}
