0fb05cd1554cf507a0e35bc979b58dee8d9f8148
FARHEEN NAAZ <farheenreshma577@gmail.com>
2026-09-24 22:54:41 +0530

    Time: 2 ms (99.17%), Space: 122.5 MB (57.77%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+        for( int i :nums){
+            if(i<=first){
+                first=i;
+            }
+            else if(i>first && i<=second){
+                second=i;
+            }
+            else{
+                return true ;
+            }
+            
+        }
+        return false;
+    }
+}
